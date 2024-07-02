b033e00d04a347196dbf97995809c9639d2a2633
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2024-07-02 13:01:53 +0200

    feat: migrate to generic v6.0.1 and prepare for publishing in maven central

--- src/main/java/ch/sbb/polarion/extension/json/editor/rest/JsonEditorRestApplication.java
+++ src/main/java/ch/sbb/polarion/extension/json/editor/rest/JsonEditorRestApplication.java
@@ -1,8 +1,6 @@
 package ch.sbb.polarion.extension.json.editor.rest;
 
 import ch.sbb.polarion.extension.generic.rest.GenericRestApplication;
-import ch.sbb.polarion.extension.generic.rest.controller.NamedSettingsApiController;
-import ch.sbb.polarion.extension.generic.rest.controller.NamedSettingsInternalController;
 import ch.sbb.polarion.extension.json.editor.rest.controller.ApiController;
 import ch.sbb.polarion.extension.json.editor.rest.controller.InternalController;
 import org.jetbrains.annotations.NotNull;
@@ -12,14 +10,11 @@ import java.util.Set;
 public class JsonEditorRestApplication extends GenericRestApplication {
 
     @Override
-    @NotNull
-    protected Set<Class<?>> getControllerClasses() {
-        final Set<Class<?>> controllerClasses = super.getControllerClasses();
-        controllerClasses.remove(NamedSettingsApiController.class);
-        controllerClasses.remove(NamedSettingsInternalController.class);
-        controllerClasses.add(ApiController.class);
-        controllerClasses.add(InternalController.class);
-        return controllerClasses;
+    protected @NotNull Set<Object> getExtensionControllerSingletons() {
+        return Set.of(
+                new ApiController(),
+                new InternalController()
+        );
     }
 
 }
