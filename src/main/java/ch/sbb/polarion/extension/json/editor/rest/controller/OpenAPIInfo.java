a75e4e12415947eb0ffe926fba3cb6ba77ef9c53
Pavel Bezliapovich <pavel.bezliapovich@sbb.ch>
2024-10-10 16:22:51 +0200

    chore: Extended OpenApi Annotation
    Refs: #26

---
+++ src/main/java/ch/sbb/polarion/extension/json/editor/rest/controller/OpenAPIInfo.java
@@ -0,0 +1,17 @@
+package ch.sbb.polarion.extension.json.editor.rest.controller;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "JSON Editor REST API",
+                version = "v1",
+                description = "",
+                termsOfService = "",
+                contact = @Contact(name = "", url = ""),
+                license = @License(name = "", url = "")))
+public class OpenAPIInfo {
+}
